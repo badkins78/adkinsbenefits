541cf4e26d7d09faeccdd3ab11dd76eebeaafbd5
Bill Adkins <badkins21878@gmail.com>
2023-02-19 10:40:24 -0600

    image resizing

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ async function imageShortcode(src, alt, className, loading, sizes = '(max-width:
 
   // create the metadata for an optimised image
   let metadata = await Image(`${src}`, {
-    widths: [200, 400, 850, 1920, 2500],
+    widths: [200, 250, 400, 850, 1920, 2500],
     formats: ['webp', 'jpeg'],
     urlPath: '/images/',
     outputDir: './public/images',
